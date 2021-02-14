603d689bc6010c9b4ae6f5e65707ef0184cccd57
Fengdi Liu <f3liu@ucsd.edu>
2021-02-13 20:51:15 -0800

    change

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,9 @@ window.addEventListener('DOMContentLoaded', () => {
   // 1. fetch API to get data
 
   fetch('https://fakestoreapi.com/products')
-  .then(response => response.json())
+  .then(res => res.json())
   .then(data => console.log(data))
+  .catch(error => console.log('ERROR'))
 
   // 2. Save data to localStorage
 
