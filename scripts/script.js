005817f1389bdd608107c851ec97a4b132c4b1db
Fengdi Liu <f3liu@ucsd.edu>
2021-02-13 20:57:42 -0800

    change

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,13 @@ window.addEventListener('DOMContentLoaded', () => {
   // 1. fetch API to get data
 
   fetch('https://fakestoreapi.com/products')
-  .then(res => res.json())
+  .then (res => res.json())
   .then(data => console.log(data))
   .catch(error => console.log('ERROR'))
 
   // 2. Save data to localStorage
-
+   
+  
 
 
 });
